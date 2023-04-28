762fa317fbd43ca603bddbe2aa46cb1ce1fb2b01
Anmol-Dodwad <56391018+Anmol-Dodwad@users.noreply.github.com>
2023-04-28 22:20:42 +0530

    Progress1

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList';
+import CookingImg from './Cooking.jpg';
 
 function App() {
   return (
-    
     <body>
       <header>
-        <h1>Ravenous</h1>
+        <h1>ravenous</h1>
       </header>
+      <div id='searchImage' style={{backgroundImage: `url(${CookingImg})`}}>
+        <div id='rating'>
+            <h6>Best<br/>Match</h6>
+            <h6>Highest<br/>Rated</h6>
+            <h6>Most<br/>Reviewed</h6>
+        </div>
+        <div id='line'></div>
+        <div id='searchBar'>
+          <input type='text' placeholder='Search Businesses'/>
+          <input type='text' placeholder='Where?'/>
+        </div>
+      </div>
       <BusinessList/>
     </body>
-    
   );
 }
 
